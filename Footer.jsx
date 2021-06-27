639bb15a948db4fbae236151095be77901063897
Amol1410 <86554828+Amol1410@users.noreply.github.com>
2021-06-27 16:48:25 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function footer() {
+  return (
+    <footer>
+      <p>Copyright By AJ @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default footer;
---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>React.js</h1>
+      <p>A JavaScript library for building interactive user interfaces.</p>
+    </div>
+  );
+}
+
+export default Info;
